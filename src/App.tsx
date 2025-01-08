901f30ed56f3495313ccdd367d3614a9b12d1e23
Lucas Castro <lucas.castro@radek.com.br>
2025-01-08 14:43:16 -0300

    fix: update env

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Suspense } from "react";
-import { useRoutes, Routes, Route, Navigate } from "react-router-dom";
+import { Suspense, useEffect } from "react";
+import { useRoutes, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Home from "./components/home";
 import Login from "./components/auth/login";
 import OrganizationView from "./components/dashboard/OrganizationView";
 import RegisterCostRecord from "./components/driver/RegisterCostRecord";
+import { supabase } from "./lib/supabase";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const checkAuth = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+      if (session) {
+        navigate("/dashboard");
+      }
+    };
+
+    checkAuth();
+  }, [navigate]);
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <>
